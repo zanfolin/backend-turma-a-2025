2ae523061219697a89629698ff8ad457858aaa9c
Graziani Vasconcelos Zanfolin <54035931+zanfolin@users.noreply.github.com>
2025-03-19 13:56:56 +0000

    Update Payment Controller

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,11 +1,51 @@
+import {z} from "zod"
+
+const PaymentSchema = z.object({
+    data: z.string().datetime(),
+    valor: z.number().positive(),
+    numero: z.number().int().positive(),
+    observacao: z.string().optional()
+});
+
 const PaymentController = {
     async createPayment(req, res) {
         try {
+            const {nome, email, senha} = req.body;
+            PaymentSchema.parse({nome, email, senha});
+            console.log({nome, email, senha});
             res.status(201).json({ message: 'Payment created successfully' });
         } catch (error) {
+            if (error instanceof z.ZodError) {
+                return res.status(400).json({
+                    message: "Erro de validação",
+                    errors: error.errors.map(
+                        err => ({
+                            atributo: err.path[0],
+                            message: err.message
+                        })
+                    )
+                })
+            }
             res.status(500).json({ message: error.message });
         }
-    }
+    },
+
+    async updatePayment(req, res) {
+        try {
+            const {id} = req.params;
+            const {valor, numero, data, observacao} = req.body;
+            PaymentSchema.parse({valor, numero, data, observacao});
+            res.status(200).json({ message: 'Payment updated successfully',
+                                   data: {id, valor, numero, data, observacao}});
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                return res.status(400).json({message: "Validation error",
+                                     details: error.errors});
+            }
+            return res.status(500).json({ message: error.message });
+        }
+    },
+
 }
 
 export default PaymentController;
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 import PaymentController from '../controllers/payment.controller.js';
 
 router.post('/payment', PaymentController.createPayment);
+router.patch('/payment/:id', PaymentController.updatePayment);
 
 export default router;
 
